d87ad54c9ebcc67140344caa6df795e5f559f687
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-09-22 16:17:51 +0100

    Module_03 ex01

---
+++ Module_03/ex01/ClapTrap.cpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 10:55:51 by bbrahim           #+#    #+#             */
+/*   Updated: 2022/09/22 16:04:38 by bbrahim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"ClapTrap.hpp"
+
+ClapTrap::ClapTrap() : hitpoint(10), energypoint(10), attackdamage(0) 
+{
+     std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap( std::string name ): name(name) 
+{
+    std::cout << "name constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap( const ClapTrap &obj)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    name = obj.name;
+    hitpoint = obj.hitpoint;
+    energypoint = obj.energypoint;
+    attackdamage = obj.attackdamage;
+}
+
+ClapTrap & ClapTrap::operator = ( const ClapTrap &obj )
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &obj)
+    {
+        name = obj.name;
+        hitpoint = obj.hitpoint;
+        energypoint = obj.energypoint;
+        attackdamage = obj.attackdamage;
+    }
+    return *this;
+}
+
+void    ClapTrap::attack(const std::string& target)
+{
+    if (hitpoint == 0)
+    {
+        std::cout << "No hit points" << std::endl;
+        return ;
+    }
+    else if (energypoint == 0)
+    {
+        std::cout << "No energy points" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap" << name << "attacks " << target << ", causing " << attackdamage << " points of damage!" << std::endl;
+    energypoint--;
+    std::cout << "energie point of atack " << energypoint << std::endl;
+}
+
+void    ClapTrap::takeDamage(unsigned int amount)
+{
+    std::cout << "ClapTrap" << name << ", loosing " << amount << " points of damage!" << std::endl;
+    attackdamage = -amount;
+}
+
+void    ClapTrap::beRepaired(unsigned int amount)
+{
+    if (energypoint == 0)
+    {
+        std::cout << "No energy points" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap" << name << ", repaired whith " << amount << " points" << std::endl;
+    hitpoint = +amount;
+    std::cout << "hit point of beRepaired " << hitpoint << std::endl;
+    energypoint--; 
+    std::cout << "energie point of beRepaired " << energypoint << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{ 
+    std::cout << "Destructor called" << std::endl;
+}
