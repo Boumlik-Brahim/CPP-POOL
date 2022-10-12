c7f85fe64cb0799f5ec7f1586086311cb429834f
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-10-12 18:16:52 +0100

    ex00 Module_07

---
+++ Module_07/ex00/whatever.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 16:23:40 by bbrahim           #+#    #+#             */
+/*   Updated: 2022/10/12 18:16:13 by bbrahim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+# include <string> 
+
+
+template <typename T>
+void swap(T &a,T &b )
+{
+    T tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T a,T b)
+{
+    if(a >= b)
+        return(b);
+    else
+        return(a);
+}
+
+template <typename T>
+T max(T a,T b)
+{
+    if(a >= b)
+        return(a);
+    else
+        return(b);
+}
+
+#endif
