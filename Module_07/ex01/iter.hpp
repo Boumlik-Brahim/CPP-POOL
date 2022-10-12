bff2b6dec692d0612ebd6ffaa5fb47faa3c5beb2
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-10-12 18:39:51 +0100

    Module_07

---
+++ Module_07/ex01/iter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 18:19:41 by bbrahim           #+#    #+#             */
+/*   Updated: 2022/10/12 18:28:03 by bbrahim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+# include <string> 
+
+template <typename I>
+I iter(I arayaddress, I arraylenght, I function)
+{
+    
+}
+
+#endif
