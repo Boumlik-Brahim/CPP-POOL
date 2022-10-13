18dc6a9c1820afdca03a066e125f66fc053ca2d7
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-10-13 17:31:23 +0100

    ex02 Module_07

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 18:30:16 by bbrahim           #+#    #+#             */
-/*   Updated: 2022/10/12 18:30:51 by bbrahim          ###   ########.fr       */
+/*   Updated: 2022/10/13 17:30:57 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,71 @@
 # include <iostream>
 # include <string> 
 
+template <typename T> class Array 
+{
+    private:
+        T   *array;
+        int size;
+    public:
+        Array();
+        Array( unsigned int n );
+        Array( const Array &copy );
 
+        Array & operator = ( const Array &assign );
+
+        int & operator[](int index);
+
+        int size();
+        
+        ~Array();
+};
+
+template <typename T> Array<T>::Array()
+{
+    array = nullptr;
+}
+
+template <typename T> Array<T>::Array( unsigned int n )
+{
+    array = new T[n]; 
+}
+
+template <typename T> Array<T>::Array( const Array &copy )
+{
+    
+}
+
+template <typename T> Array & Array<T>::operator = ( const Array &assign )
+{
+    if(this != &assign)
+    {
+        
+    }
+    return(*this);
+}
+
+template <typename T> int Array<T>::size()
+{
+    int i = 0;
+    while (array[i])
+    {
+        i++;
+    }
+    return(i);
+}
+
+template <typename T> int& Array::operator[](int index)
+{
+   if (index >= this->size) 
+   {
+        throw
+   }
+   return array[index];
+}
+
+template <typename T> Array<T>::Array()
+{
+    delete [] T;
+}
 
 #endif
