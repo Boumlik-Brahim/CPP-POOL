7fb303d93627347fbe8a5cdf0f431692b71739e7
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-10-17 18:54:01 +0100

    ex02 main

--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -6,13 +6,42 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 18:52:47 by bbrahim           #+#    #+#             */
-/*   Updated: 2022/10/16 18:55:04 by bbrahim          ###   ########.fr       */
+/*   Updated: 2022/10/17 18:53:47 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"MutantStack.hpp"
 
-int main(int argc, char const *argv[])
+int main()
 {
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
     return 0;
 }
